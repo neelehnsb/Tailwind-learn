8692249f0f995856c2f1e232c41d9f67197ae2d2
Neelesh Bharadwaj <19uec104@lnmiit.ac.in>
2023-06-28 14:32:51 +0530

    Complete Microsoft using Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {
+      fontSize:{
+        xxs : ['11px', '40px'],
+        ts : ['14px', '40px']
+      },
+
       colors: {
-        blue_btn : "#0067b8"
+        blue_btn : "#0067b8",
+        link_bg : "#f2f2f2"
       }
     },
   },
